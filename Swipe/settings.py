367cc326fb2cecbbbe0d1a9d82465e9788657491
Dima <109269676+Sampleeeees@users.noreply.github.com>
2023-07-02 00:24:40 +0300

    Update settings.py

--- Swipe/settings.py
+++ Swipe/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['194.146.38.190']
+ALLOWED_HOSTS = ['83.229.83.250']
 
 
 # Application definition
